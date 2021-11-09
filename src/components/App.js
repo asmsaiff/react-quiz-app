19128e425c67db9d76c51fe6990838ac9502c945
saidmsaif <saidmsaiff@gmail.com>
2021-11-09 22:55:37 +0600

    quiz components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,11 @@
 import '../styles/App.css'
 import Layout from "./Layout";
-import HomePage from './pages/Home';
 import Quiz from './pages/Quiz';
-// import Videos from './Videos';
+import Videos from './Videos';
 
 function App() {
     return (
         <Layout>
-            <HomePage />
             {/* <Videos /> */}
             <Quiz />
         </Layout>
