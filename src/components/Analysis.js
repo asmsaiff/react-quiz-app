ba85013650361279489e62b6cdc66bc3f9ab56f0
saidmsaif <saidmsaiff@gmail.com>
2021-11-12 19:24:19 +0600

    analysis component

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -1,8 +1,10 @@
+import classes from "../styles/Analysis.module.css";
+
 export default function Analysis() {
     return (
-        <div class="analysis">
-          <h1>Question Analysis</h1>
-            <h4>You answered 5 out of 10 questions correctly</h4>
-        </div>
-    )
+		<div className={classes.analysis}>
+			<h1>Question Analysis</h1>
+			<h4>You answered 5 out of 10 questions correctly</h4>
+		</div>
+	);
 }
