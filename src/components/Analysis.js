75032e40597313e7e2059a20cd79a9a24a9bffce
saidmsaif <saidmsaiff@gmail.com>
2021-11-12 17:52:03 +0600

    analysis component

---
+++ src/components/Analysis.js
@@ -0,0 +1,8 @@
+export default function Analysis() {
+    return (
+        <div class="analysis">
+          <h1>Question Analysis</h1>
+            <h4>You answered 5 out of 10 questions correctly</h4>
+        </div>
+    )
+}
