4aab1724a8e200730520e19730c51a77bd8508ac
saidmsaif <saidmsaiff@gmail.com>
2021-11-12 19:24:48 +0600

    bind class with template string

--- src/components/FormInput/AnswerCheckBox.js
+++ src/components/FormInput/AnswerCheckBox.js
@@ -3,7 +3,7 @@ import classes from "../../styles/FormInput/AnswerCheckBox.module.css";
 export default function AnswerCheckBox() {
     return (
 		<>
-			<label class={classes.answer} for='option9'>
+			<label class={`${classes.answer}`} for='option9'>
 				<input type='checkbox' id='option9' />A New Hope 1
 			</label>
 		</>
