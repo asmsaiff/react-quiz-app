f96c0c22b39131c015bf6594b43dd6bf7a432753
saidmsaif <saidmsaiff@gmail.com>
2022-01-31 22:20:30 +0600

    add custom css and header component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import '../assets/css/tailwind.css'
+import "../assets/css/custom.css";
+import Header from './Header';
 
 export class App extends Component {
-  render() {
-      return <div>
-        <h1 className='text-2xl'>This is a app</h1>
-    </div>;
-  }
+    render() {
+        return (
+			<>
+				<section className='min-h-screen text-lime-50 bg-slate-900'>
+					<div className='md:w-10/12 mx-auto'>
+						<Header />
+					</div>
+				</section>
+			</>
+		);
+    }
 }
 
 export default App;
