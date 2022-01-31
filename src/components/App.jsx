32b25f2d90c685d422389f83cf8fd7612adcdd71
saidmsaif <saidmsaiff@gmail.com>
2022-01-31 23:49:15 +0600

    add question nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../assets/css/tailwind.css'
 import "../assets/css/custom.css";
 import Header from './Header';
+import QuestionNav from './QuestionNav';
 
 export class App extends Component {
     render() {
@@ -9,7 +10,8 @@ export class App extends Component {
 			<>
 				<section className='min-h-screen text-lime-50 bg-slate-900 flex items-center'>
 					<div className='md:w-10/12 mx-auto'>
-						<Header />
+                        <Header />
+                        <QuestionNav />
 					</div>
 				</section>
 			</>
