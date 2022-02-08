0b1b70818de1ae7f6beef25ddc08950f79432ee3
saifullahsiddique <info@saifullah.co>
2022-02-09 02:07:34 +0600

    responsive design fix and delete old files and add all code to app

---
+++ src/data/Questions.js
@@ -0,0 +1,212 @@
+export const questions = [
+	{
+		questionText: "What is WordPress?",
+		options: [
+			{
+				option: "WordPress is a server",
+				isCorrect: false,
+			},
+			{
+				option: "WordPress is a programming language",
+				isCorrect: false,
+			},
+			{
+				option: "JavaScript is a Zoo",
+				isCorrect: false,
+			},
+			{
+				option: "WordPress is a Content Management System",
+				isCorrect: true,
+			},
+		],
+	},
+	{
+		questionText: "How many user roles available in wordpress first installation?",
+		options: [
+			{
+				option: "4",
+				isCorrect: false,
+			},
+			{
+				option: "50",
+				isCorrect: false,
+			},
+			{
+				option: "5",
+				isCorrect: true,
+			},
+			{
+				option: ".5",
+				isCorrect: false,
+			},
+		],
+	},
+	{
+		questionText: "What is XAMPP?",
+		options: [
+			{
+				option: "XAMPP is a operating system",
+				isCorrect: false,
+			},
+			{
+				option: "XAMPP is a fruits",
+				isCorrect: false,
+			},
+			{
+				option: "I don't even know!",
+				isCorrect: false,
+			},
+			{
+				option: "XAMPP is a free and open-source cross-platform web server solution",
+				isCorrect: true,
+			},
+		],
+	},
+	{
+		questionText: "What does it mean by a theme in wordpress?",
+		options: [
+			{
+				option: "Themes are used for making orange juice.",
+				isCorrect: false,
+			},
+			{
+				option: "Themes are used for building bridge.",
+				isCorrect: false,
+			},
+			{
+				option: "Themes defines website structures and designs.",
+				isCorrect: true,
+			},
+			{
+				option: "All of the above.",
+				isCorrect: false,
+			},
+		],
+	},
+	{
+		questionText: "What is plugins?",
+		options: [
+			{
+				option: "Plugins is a super computer.",
+				isCorrect: false,
+			},
+			{
+				option: "plugin is a software add-on that is installed on a program, enhancing its functionalities.",
+				isCorrect: true,
+			},
+			{
+				option: "A car name",
+				isCorrect: false,
+			},
+			{
+				option: "Database",
+				isCorrect: false,
+			},
+		],
+	},
+	{
+		questionText: "Author can _____________?",
+		options: [
+			{
+				option: "Edit it's won post.",
+				isCorrect: true,
+			},
+			{
+				option: "Delete post",
+				isCorrect: false,
+			},
+			{
+				option: "Upload image",
+				isCorrect: false,
+			},
+			{
+				option: "Nothing",
+				isCorrect: false,
+			},
+		],
+	},
+	{
+		questionText: "Editor can ____________?",
+		options: [
+			{
+				option: "Uninstall wordpress",
+				isCorrect: false,
+			},
+			{
+				option: "Update themes and plugins",
+				isCorrect: false,
+			},
+			{
+				option: "Can play football with cricket ball.",
+				isCorrect: false,
+			},
+			{
+				option: "Make a page, Create, Read, Update, Delete Post",
+				isCorrect: true,
+			},
+		],
+	},
+	{
+		questionText: "What is permalink?",
+		options: [
+			{
+				option: "I saw it for the first time since my birth.",
+				isCorrect: false,
+			},
+			{
+				option: "Page or post link which is displayed in browser address bar.",
+				isCorrect: true,
+			},
+			{
+				option: "A snake name in ludo-club",
+				isCorrect: false,
+			},
+			{
+				option: "All of the above.",
+				isCorrect: false,
+			},
+		],
+	},
+	{
+		questionText: "Whose are the post category??",
+		options: [
+			{
+				option: "Post Title, Appearance, Dashboard, Menus",
+				isCorrect: false,
+			},
+			{
+				option: "Lorem ipsum dolor sit amet.",
+				isCorrect: false,
+			},
+			{
+				option: "WordPress, Web Development, Programming, Sports, Hobby",
+				isCorrect: true,
+			},
+			{
+				option: "All of the above",
+				isCorrect: false,
+			},
+		],
+	},
+	{
+		questionText: "WordPress can not make all kind of websites?",
+		options: [
+			{
+				option: "HA HA HA HA HA HA HA HA HA HA HA",
+				isCorrect: true,
+			},
+			{
+				option: "Stop, stop, I can't lough anymore!!",
+				isCorrect: true,
+			},
+			{
+				option: "পাড়ায় আসিস, বার্নিশ করে ছেড়ে দেব।",
+				isCorrect: true,
+			},
+			{
+				option: "May be!!!!!!!!!!!!",
+				isCorrect: true,
+			},
+		],
+	},
+];
