4fd786745c42ec415928f7012c3da29d1e8a2632
saidmsaif <saidmsaiff@gmail.com>
2022-02-09 09:22:02 +0600

    add result component and conditional check for result block

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,15 +8,15 @@ import { questions } from "../data/Questions";
 
 export class App extends Component {
 	state = {
+		examName: "WordPress Basic",
+		examTime: "10 Minutes",
+		fullMarks: "10",
+		passMarks: "8",
 		currentQuestion: 0,
 		score: 0,
 		showScore: true,
 		totalQuestion: questions.length,
 		nextButton: "",
-		examName: "WordPress Basic",
-		examTime: "10 Minutes",
-		fullMarks: "10",
-		passMarks: "8",
 	};
 
 	setExamInfo = (props) => {
@@ -64,89 +64,103 @@ export class App extends Component {
 		return (
 			<>
 				<section className='min-h-screen text-lime-50 bg-slate-900 flex items-center py-12 px-4'>
-					<div className='md:w-10/12 mx-auto'>
-						{/* Header */}
-						<h1 className='inter font-extrabold text-primary text-3xl lg:text-5xl text-center'>
-							{this.state.examName}
-						</h1>
+					{this.state.nextQuestion < questions.length ? (
+						<div className='md:w-10/12 mx-auto'>
+							{/* Header */}
+							<h1 className='inter font-extrabold text-primary text-3xl lg:text-5xl text-center'>
+								{this.state.examName}
+							</h1>
 
-						<div className='flex flex-col sm:flex-row text-center justify-center mt-4 sm:divide-x divide-gray-500 space-y-2 sm:space-y-0'>
-							<div className='flex sm:pr-4'>
-								<RiTimeLine className='text-primary self-center mr-2' />
-								<span className='text-sm'>
-									Total Marks: {this.state.fullMarks}
-								</span>
-							</div>
-							<div className='flex sm:px-4'>
-								<RiTimeLine className='text-primary self-center mr-2' />
-								<span className='text-sm'>
-									Pass Marks: {this.state.passMarks}
-								</span>
-							</div>
-							<div className='flex sm:pl-4'>
-								<RiTimeLine className='text-primary self-center mr-2' />
-								<span className='text-sm'>
-									Time: {this.state.examTime}
-								</span>
-							</div>
-						</div>
-
-						{/* Question Navigation */}
-						<div className='flex justify-between items-center mt-8 mb-6'>
-							<div>
-								<h2 className='text-sm text-secondary'>
-									Question
-									<span className='text-4xl md:text-5xl text-primary'>
-										{this.state.currentQuestion + 1}
-									</span>{" "}
-									out of{" "}
-									<span className='text-4xl md:text-5xl text-primary'>
-										{this.state.totalQuestion}
+							<div className='flex flex-col sm:flex-row text-center justify-center mt-4 sm:divide-x divide-gray-500 space-y-2 sm:space-y-0'>
+								<div className='flex sm:pr-4'>
+									<RiTimeLine className='text-primary self-center mr-2' />
+									<span className='text-sm'>
+										Total Marks: {this.state.fullMarks}
 									</span>
-								</h2>
-							</div>
-							<button onClick={this.navigateQuestion} className='text-2xl font-extrabold flex items-center gap-2 text-primary border-bottom'>
-								<span>Next</span>
-								<CgArrowRight />
-							</button>
-						</div>
-
-						{/* Question */}
-						<div className='space-y-6'>
-							<div className='flex text-xl sm:text-4xl text-primary space-x-4'>
-								<span className='paris font-bold'>Q.</span>
-								<span>
-									{
-										questions[this.state.currentQuestion].questionText
-									}
-								</span>
-							</div>
-							<div className='flex text-xl sm:text-4xl text-primary space-x-4'>
-								<span className='paris font-bold'>A.</span>
-								<span></span>
+								</div>
+								<div className='flex sm:px-4'>
+									<RiTimeLine className='text-primary self-center mr-2' />
+									<span className='text-sm'>
+										Pass Marks: {this.state.passMarks}
+									</span>
+								</div>
+								<div className='flex sm:pl-4'>
+									<RiTimeLine className='text-primary self-center mr-2' />
+									<span className='text-sm'>
+										Time: {this.state.examTime}
+									</span>
+								</div>
 							</div>
 
-							<div className='flex flex-col space-y-4'>
-								{questions[
-									this.state.currentQuestion
-								].options.map((ansOptions) => {
-									return (
-										<button
-											onClick={() => {
-												this.handleAnswerBtnClick(
-													ansOptions.isCorrect,
-												);
-											}}
-											className='flex bg-third items-center text-left gap-4 py-3 px-2 sm:px-6 text-sm md:text-xl ans'
-										>
-											<BiLogInCircle className='question-icon text-primary' />
-											<span>{ansOptions.option}</span>
-										</button>
-									);
-								})}
+							{/* Question Navigation */}
+							<div className='flex justify-between items-center mt-8 mb-6'>
+								<div>
+									<h2 className='text-sm text-secondary'>
+										Question
+										<span className='text-4xl md:text-5xl text-primary'>
+											{this.state.currentQuestion + 1}
+										</span>{" "}
+										out of{" "}
+										<span className='text-4xl md:text-5xl text-primary'>
+											{this.state.totalQuestion}
+										</span>
+									</h2>
+								</div>
+								<button
+									onClick={this.navigateQuestion}
+									className='text-2xl font-extrabold flex items-center gap-2 text-primary border-bottom'
+								>
+									<span>Next</span>
+									<CgArrowRight />
+								</button>
+							</div>
+
+							{/* Question */}
+							<div className='space-y-6'>
+								<div className='flex text-xl sm:text-4xl text-primary space-x-4'>
+									<span className='paris font-bold'>Q.</span>
+									<span>
+										{
+											questions[
+												this.state.currentQuestion
+											].questionText
+										}
+									</span>
+								</div>
+								<div className='flex text-xl sm:text-4xl text-primary space-x-4'>
+									<span className='paris font-bold'>A.</span>
+									<span></span>
+								</div>
+
+								<div className='flex flex-col space-y-4'>
+									{questions[
+										this.state.currentQuestion
+									].options.map((ansOptions) => {
+										return (
+											<button
+												onClick={() => {
+													this.handleAnswerBtnClick(
+														ansOptions.isCorrect,
+													);
+												}}
+												className='flex bg-third items-center text-left gap-4 py-3 px-2 sm:px-6 text-sm md:text-xl ans'
+											>
+												<BiLogInCircle className='question-icon text-primary' />
+												<span>{ansOptions.option}</span>
+											</button>
+										);
+									})}
+								</div>
 							</div>
 						</div>
-					</div>
+					) : (
+						<div className='md:w-10/12 mx-auto'>
+							{/* Header */}
+							<h1 className='inter font-extrabold text-primary text-3xl lg:text-5xl text-center'>
+								{this.state.score}
+							</h1>
+						</div>
+					)}
 				</section>
 			</>
 		);
